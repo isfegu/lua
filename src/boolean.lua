76256cb8d1df6fc581b8464c92be03abaad98503
Israel Fenor <israelfenor@gmail.com>
2016-06-23 21:05:50 +0200

    Ejemplos de boleanos y coercions

---
+++ src/boolean.lua
@@ -0,0 +1,51 @@
+-- How boolean works
+
+var = nil
+
+if var == true then
+    print("nil is true")
+end
+
+if var == false then
+    print("nil is false")
+end
+
+true_var = true
+
+if true_var == true then
+    print("true_var is true")
+end
+
+if true_var == false then
+    print("true_var is false")
+end
+
+false_var = false
+
+if false_var == true then
+    print("false_var is true")
+end
+
+if false_var == false then
+    print("false_var is false")
+end
+
+zero_var = 0
+
+if zero_var == true then
+    print("zero_var is true")
+end
+
+if zero_var == false then
+    print("zero_var is false")
+end
+
+empty_var = ''
+
+if empty_var == true then
+    print("empty_var is true")
+end
+
+if empty_var == false then
+    print("empty_var is false")
+end
---
+++ src/coercions.lua
@@ -0,0 +1,7 @@
+print("10" + 1)
+print("10 + 1")
+print("-5.3e-10" * "2")
+print(10 .. 20) -- spaces are important
+print(tostring(10) == '10')
+print(10 .. "" == "10")
+-- print("hello" + 2) -- Error
